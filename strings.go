4cef00a2d9ea862ae2d33222679216219ed322ca
Taras Shumeiko <shumeyko@gmail.com>
2020-11-05 12:36:55 +0200

    add IsStringUrl function

--- strings.go
+++ strings.go
@@ -1,5 +1,17 @@
 package assist
 
+import (
+	"net/url"
+)
+
+func IsStringUrl(v string) bool {
+	url, err := url.ParseRequestURI(v)
+	if err != nil {
+		return false
+	}
+	return url != nil && url.IsAbs()
+}
+
 func IsArrayContainString(list *[]string, key string) bool {
 	if list != nil {
 		for _, v := range *list {
